74ca96b17a3b7095f04d71c545ddb1ae95b06c1a
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2023-12-26 11:05:58 +0530

    commit 10 ish sports

---
+++ app/src/main/java/com/example/argon/Adapters/SportsAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.argon.Adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.argon.Fragments.SportFragment.Men
+import com.example.argon.Fragments.SportFragment.Women
+
+class SportsAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                Men()
+            }
+            else -> {
+                Women()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/argon/Fragments/MainFragment/Sports.kt
+++ app/src/main/java/com/example/argon/Fragments/MainFragment/Sports.kt
@@ -5,14 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.argon.Adapters.MatchesAdapter
+import com.example.argon.Adapters.SportsAdapter
 import com.example.argon.R
+import com.example.argon.databinding.FragmentSportBinding
+import com.google.android.material.tabs.TabLayout
 
 class Sports : Fragment() {
+    private lateinit var binding: FragmentSportBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sport, container, false)
+    ): View {
+        binding = FragmentSportBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addTab(R.drawable.male)
+        addTab(R.drawable.female)
+
+        binding.viewPager.adapter = SportsAdapter(childFragmentManager, lifecycle)
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    binding.viewPager.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tabLayout.selectTab(binding.tabLayout.getTabAt(position))
+            }
+        })
+    }
+
+    private fun addTab(image: Int) {
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setIcon(image))
     }
 }
